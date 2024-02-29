e67dd5e00ee8a7f5482bc78c33c9aa78b8c311ec
Aditya <adityajanuarizki4@gmail.com>
2024-02-29 11:36:14 +0700

    Menambaah List barang controller

---
+++ resources/views/list_barang.blade.php
@@ -0,0 +1,7 @@
+<html>
+<div>
+    <h1>List barang</h1>
+    <p>Kode barang: {{$id}}</p>
+    <p>Nama barang: {{$nama}}</p>
+</div>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController; // Perhatikan penggunaan huruf besar pada kata 'Use' yang diubah menjadi 'use'
-
+use App\Http\Controllers\ListBarangController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -31,3 +31,4 @@ Route::prefix('admin')->group(function () {
         return 'admin users';
     });
 });
+Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
