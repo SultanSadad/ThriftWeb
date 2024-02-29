3b27e20f67275fd1c67d4bbe221953ae44435c64
Aditya <adityajanuarizki4@gmail.com>
2024-02-29 09:46:17 +0700

    Edit Web.php p3

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,18 @@ use App\Http\Controllers\HomeController; // Perhatikan penggunaan huruf besar pa
 
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
+Route::get('/welcome', function (){
+    return view('welcome');
+});
+Route::get('/user/{id}', function (int $id) {
+    return "User dengan ID " . $id;
+});
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return 'admin dashboard';
+    });
+});
+
+Route::get('/users', function (){
+    return 'admin user';
+});
